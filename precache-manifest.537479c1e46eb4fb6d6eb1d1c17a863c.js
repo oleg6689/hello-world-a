1c5a97ccd3e61b1e1848df5d577de0fc84cd9a5e
oleg6689 <oleg-gorbunov-89@mail.ru>
2020-04-13 18:56:02 +0500

    Updates

---
+++ precache-manifest.537479c1e46eb4fb6d6eb1d1c17a863c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26f26b3a6f4a828a23c6191beec586a8",
+    "url": "./index.html"
+  },
+  {
+    "revision": "0ef4128b25e03af122a9",
+    "url": "./static/css/2.86df01db.chunk.css"
+  },
+  {
+    "revision": "e41554408e0ea9607d60",
+    "url": "./static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "0ef4128b25e03af122a9",
+    "url": "./static/js/2.45827959.chunk.js"
+  },
+  {
+    "revision": "21ced859ea2b2d6b856d461ad6c2afed",
+    "url": "./static/js/2.45827959.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e41554408e0ea9607d60",
+    "url": "./static/js/main.26933983.chunk.js"
+  },
+  {
+    "revision": "88f7e0bc6b03921ebc9a",
+    "url": "./static/js/runtime-main.77727976.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "./static/media/persik.4e1ec840.png"
+  }
+]);
